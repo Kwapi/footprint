2f3ed479f23bed9d356567956efbfb8a07a01037
Ryan Holder <mail@ryanholder.co.uk>
2017-11-24 20:31:19 +0000

    Starts to homepage

--- resources/views/email_form.blade.php
+++ resources/views/email_form.blade.php
@@ -1,13 +1,15 @@
 @extends('master')
 
 @section('content')
-    <form action="/fullcontact/email" method="POST">
-        <div class="form-group">
-            <label for="exampleInputEmail1">Email address</label>
-            <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+    <section class="min-h-screen flex items-center justify-center">
+        <div class="w-full max-w-md">
+            <h2 class="mb-2">Discover your digital footprint</h1>
+            <form action="/fullcontact/email" method="POST" class="text-center">
+                <div class="form-group">
+                    <input type="email" name="email" class="shadow border rounded py-2 px-3 text-grey-darker w-4/5" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                    <button type="submit" class="shadow bg-blue hover:bg-blue-dark text-white font-bold py-2 px-4 rounded w-1/6">Search</button>
+                </div>
+            </form>
         </div>
-
-        <button type="submit" class="btn btn-primary">Submit</button>
-
-    </form>
+    </section>
 @endsection
