e65cc4218764105142e01eec1504f0eb1cf8248d
Michal <m.zak08@gmail.com>
2017-11-24 15:43:25 +0000

    caching

--- app/Http/Controllers/FullContactController.php
+++ app/Http/Controllers/FullContactController.php
@@ -11,21 +11,38 @@ class FullContactController extends BaseController
     public function getInfoFromEmail(Request $request){
 
         $email = $request->input('email');
-        $header = array("X-FullContact-APIKey:".env('FULLCONTACT_API_KEY'));
         $email_encoded = urlencode($email);
-        $url = "https://api.fullcontact.com/v2/person.json?email=".$email_encoded;
 
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL,$url);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $json_file_name = $email_encoded.'.json';
+
+        $exists = Storage::disk('local')->exists($json_file_name);
+
+        if(!$exists){
+            $header = array("X-FullContact-APIKey:".env('FULLCONTACT_API_KEY'));
+            $url = "https://api.fullcontact.com/v2/person.json?email=".$email_encoded;
+
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL,$url);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
 
-        $information = curl_exec($ch);
-        curl_close($ch);
-        $json = json_encode($information);
+            $information = curl_exec($ch);
+            curl_close($ch);
+            $json = json_encode($information);
 
-        Storage::disk('local')->put($email_encoded.'.json', $information);
+
+            Storage::disk('local')->put($json_file_name, $information);
+
+            echo 'Retrieved';
+
+            echo $json;
+        }else{
+            echo 'Loaded ';
+            echo Storage::disk('local')->get($json_file_name);
+        }
+
+
 
     }
 }
